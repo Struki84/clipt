4a46d18875d78cfe8fe797ae6f0c137040ae219b
Simun Strukan <simun.strukan@gmail.com>
2025-10-08 23:17:57 +0200

    Working on filtering the menu list

--- model.go
+++ model.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/openai"
@@ -42,25 +41,20 @@ func (model *TestModel) ChatHistory(sessionID string) ChatHistory {
 }
 
 func (model *TestModel) Run(ctx context.Context, input string) error {
-	log.Printf("running with input: %v", input)
-	log.Printf("stream handler is nil: %v", model.streamHandler == nil)
-
 	content := []llms.MessageContent{
 		llms.TextParts(llms.ChatMessageTypeSystem, "You are a helpful assistant!"),
 		llms.TextParts(llms.ChatMessageTypeHuman, input),
 	}
 
-	completion, err := model.LLM.GenerateContent(ctx, content, llms.WithStreamingFunc(model.streamHandler))
+	_, err := model.LLM.GenerateContent(ctx, content, llms.WithStreamingFunc(model.streamHandler))
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
-	log.Printf("Completion: %v", completion.Choices[0].Content)
 
 	return nil
 }
 
 func (model *TestModel) Stream(ctx context.Context, callback func(ctx context.Context, chunk []byte) error) {
-	log.Printf("Set stream")
 	model.streamHandler = callback
 }
