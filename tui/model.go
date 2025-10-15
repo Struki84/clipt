64f9e13b0fe3c549b936f9c172f23577ba1ee66c
Simun Strukan <simun.strukan@gmail.com>
2025-10-16 00:32:33 +0200

    Added layout styles for chat msgs

--- tui/model.go
+++ tui/model.go
@@ -1,1 +1,28 @@
 package tui
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func Render(provider ChatProvider) {
+	file, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+	app := tea.NewProgram(
+		NewLayoutView(provider),
+		tea.WithAltScreen(),
+		tea.WithMouseCellMotion(),
+	)
+	if _, err := app.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+
+}
